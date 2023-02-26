Morfubex/nerf

commit 94f79fedb0ad61da2f76a623af8f36e22e3d7ad0

SFM: sfm-based depth, some breadcrumbs for pycolmap (#1457)
* save

* save

* more cleanup

* save

* save

* update phototourism

* save

* works

* works

* cleanup

* cleanup

* lint

* python3 default

* cleanup

scripts/process_data.py
@@ -91,10 +91,14 @@ class ProcessImages:
     """
     gpu: bool = True
     """If True, use GPU."""
+    use_sfm_depth: bool = False
+    """If True, export and use depth maps induced from SfM points."""
+    include_depth_debug: bool = False
+    """If --use-sfm-depth and this flag is True, also export debug images showing SfM overlaid upon input images."""
     verbose: bool = False
     """If True, print extra logging."""
 
-    def main(self) -> None:
+    def main(self) -> None:  # pylint: disable=R0915
         """Process images into a nerfstudio dataset."""
         # pylint: disable=too-many-statements
         require_cameras_exist = False
@@ -161,14 +165,33 @@ class ProcessImages:
             # Colmap uses renamed images
             image_rename_map = None
 
+        # Export depth maps
+        if self.use_sfm_depth:
+            depth_dir = self.output_dir / "depth"
+            depth_dir.mkdir(parents=True, exist_ok=True)
+            image_id_to_depth_path = colmap_utils.create_sfm_depth(
+                recon_dir=colmap_dir / "sparse" / "0",
+                output_dir=depth_dir,
+                include_depth_debug=self.include_depth_debug,
+                input_images_dir=image_dir,
+                verbose=self.verbose,
+            )
+            summary_log.append(
+                process_data_utils.downscale_images(
+                    depth_dir, self.num_downscales, folder_name="depths", nearest_neighbor=True, verbose=self.verbose
+                )
+            )
+        else:
+            image_id_to_depth_path = {}
+
         # Save transforms.json
         if (colmap_model_path / "cameras.bin").exists():
             with CONSOLE.status("[bold yellow]Saving results to transforms.json", spinner="balloon"):
                 num_matched_frames = colmap_utils.colmap_to_json(
-                    cameras_path=colmap_model_path / "cameras.bin",
-                    images_path=colmap_model_path / "images.bin",
+                    recon_dir=colmap_dir / "sparse" / "0",
                     output_dir=self.output_dir,
                     camera_model=CAMERA_MODELS[self.camera_type],
+                    image_id_to_depth_path=image_id_to_depth_path,
                     image_rename_map=image_rename_map,
                 )
                 summary_log.append(f"Colmap matched {num_matched_frames} images")
@@ -272,12 +295,16 @@ class ProcessVideo:
     """Create circle crop mask. The radius is the percent of the image diagonal."""
     percent_crop: Tuple[float, float, float, float] = (0.0, 0.0, 0.0, 0.0)
     """Percent of the image to crop. (top, bottom, left, right)"""
+    use_sfm_depth: bool = False
+    """If True, export and use depth maps induced from SfM points."""
+    include_depth_debug: bool = False
+    """If --use-sfm-depth and this flag is True, also export debug images showing SfM overlaid upon input images."""
     gpu: bool = True
     """If True, use GPU."""
     verbose: bool = False
     """If True, print extra logging."""
 
-    def main(self) -> None:
+    def main(self) -> None:  # pylint: disable=R0915
         """Process video into a nerfstudio dataset."""
         install_checks.check_ffmpeg_installed()
         install_checks.check_colmap_installed()
@@ -372,14 +399,33 @@ class ProcessVideo:
                 CONSOLE.log("[bold red]Invalid combination of sfm_tool, feature_type, and matcher_type, exiting")
                 sys.exit(1)
 
+        # Export depth maps
+        if self.use_sfm_depth:
+            depth_dir = self.output_dir / "depth"
+            depth_dir.mkdir(parents=True, exist_ok=True)
+            image_id_to_depth_path = colmap_utils.create_sfm_depth(
+                recon_dir=colmap_dir / "sparse" / "0",
+                output_dir=depth_dir,
+                include_depth_debug=self.include_depth_debug,
+                input_images_dir=image_dir,
+                verbose=self.verbose,
+            )
+            summary_log.append(
+                process_data_utils.downscale_images(
+                    depth_dir, self.num_downscales, folder_name="depths", nearest_neighbor=True, verbose=self.verbose
+                )
+            )
+        else:
+            image_id_to_depth_path = {}
+
         # Save transforms.json
         if (colmap_dir / "sparse" / "0" / "cameras.bin").exists():
             with CONSOLE.status("[bold yellow]Saving results to transforms.json", spinner="balloon"):
                 num_matched_frames = colmap_utils.colmap_to_json(
-                    cameras_path=colmap_dir / "sparse" / "0" / "cameras.bin",
-                    images_path=colmap_dir / "sparse" / "0" / "images.bin",
+                    recon_dir=colmap_dir / "sparse" / "0",
                     output_dir=self.output_dir,
                     camera_model=CAMERA_MODELS[self.camera_type],
+                    image_id_to_depth_path=image_id_to_depth_path,
                     camera_mask_path=mask_path,
                     image_rename_map=None,
                 )
@@ -426,6 +472,10 @@ class ProcessInsta360:
     """If True, skips COLMAP and generates transforms.json if possible."""
     colmap_cmd: str = "colmap"
     """How to call the COLMAP executable."""
+    use_sfm_depth: bool = False
+    """If True, export and use depth maps induced from SfM points."""
+    include_depth_debug: bool = False
+    """If --use-sfm-depth and this flag is True, also export debug images showing SfM overlaid upon input images."""
     gpu: bool = True
     """If True, use GPU."""
     verbose: bool = False
@@ -493,14 +543,33 @@ class ProcessInsta360:
                 colmap_cmd=self.colmap_cmd,
             )
 
+        # Export depth maps
+        if self.use_sfm_depth:
+            depth_dir = self.output_dir / "depth"
+            depth_dir.mkdir(parents=True, exist_ok=True)
+            image_id_to_depth_path = colmap_utils.create_sfm_depth(
+                recon_dir=colmap_dir / "sparse" / "0",
+                output_dir=depth_dir,
+                include_depth_debug=self.include_depth_debug,
+                input_images_dir=image_dir,
+                verbose=self.verbose,
+            )
+            summary_log.append(
+                process_data_utils.downscale_images(
+                    depth_dir, self.num_downscales, folder_name="depths", nearest_neighbor=True, verbose=self.verbose
+                )
+            )
+        else:
+            image_id_to_depth_path = {}
+
         # Save transforms.json
         if (colmap_dir / "sparse" / "0" / "cameras.bin").exists():
             with CONSOLE.status("[bold yellow]Saving results to transforms.json", spinner="balloon"):
                 num_matched_frames = colmap_utils.colmap_to_json(
-                    cameras_path=colmap_dir / "sparse" / "0" / "cameras.bin",
-                    images_path=colmap_dir / "sparse" / "0" / "images.bin",
+                    recon_dir=colmap_dir / "sparse" / "0",
                     output_dir=self.output_dir,
                     camera_model=CAMERA_MODELS["fisheye"],
+                    image_id_to_depth_path=image_id_to_depth_path,
                     image_rename_map=None,
                 )
                 summary_log.append(f"Colmap matched {num_matched_frames} images")

.dockerignore
@@ -170,7 +170,6 @@ events.out.*
 
 # Data
 data
-!*/data
 
 # Misc
 old/

Dockerfile
@@ -48,10 +48,12 @@ RUN apt-get update && \
     libsuitesparse-dev \
     nano \
     protobuf-compiler \
+    python-is-python3 \
     python3.10-dev \
     python3-pip \
     qtbase5-dev \
     sudo \
+    vim-tiny \
     wget && \
     rm -rf /var/lib/apt/lists/*
 
@@ -62,7 +64,7 @@ RUN git clone --branch v0.6.0 https://github.com/google/glog.git --single-branch
     mkdir build && \
     cd build && \
     cmake .. && \
-    make -j && \
+    make -j `nproc` && \
     make install && \
     cd ../.. && \
     rm -rf glog
@@ -76,7 +78,7 @@ RUN git clone --branch 2.1.0 https://ceres-solver.googlesource.com/ceres-solver.
     mkdir build && \
     cd build && \
     cmake .. -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF && \
-    make -j && \
+    make -j `nproc` && \
     make install && \
     cd ../.. && \
     rm -rf ceres-solver
@@ -87,13 +89,12 @@ RUN git clone --branch 3.8 https://github.com/colmap/colmap.git --single-branch 
     mkdir build && \
     cd build && \
     cmake .. -DCUDA_ENABLED=ON \
-             -DCUDA_NVCC_FLAGS="--std c++14" \
              -DCMAKE_CUDA_ARCHITECTURES=${CUDA_ARCHITECTURES} && \
-    make -j && \
+    make -j `nproc` && \
     make install && \
     cd ../.. && \
     rm -rf colmap
-    
+
 # Create non root user and setup environment.
 RUN useradd -m -d /home/user -g root -G sudo -u 1000 user
 RUN usermod -aG sudo user
@@ -112,13 +113,14 @@ SHELL ["/bin/bash", "-c"]
 
 # Upgrade pip and install packages.
 RUN python3.10 -m pip install --upgrade pip setuptools pathtools promise pybind11
-# Install pytorch and submodules (Currently, we still use cu116 which is the latest version for toch 1.12.1 and is compatible with CUDA 11.8).
+# Install pytorch and submodules (Currently, we still use cu116 which is the latest version for torch 1.12.1 and is compatible with CUDA 11.8).
 RUN python3.10 -m pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
 # Install tynyCUDNN (we need to set the target architectures as environment variable first).
 ENV TCNN_CUDA_ARCHITECTURES=${CUDA_ARCHITECTURES}
-RUN python3.10 -m pip install git+https://github.com/NVlabs/tiny-cuda-nn.git#subdirectory=bindings/torch
+RUN python3.10 -m pip install git+https://github.com/NVlabs/tiny-cuda-nn.git@v1.6#subdirectory=bindings/torch
 
 # Install pycolmap 0.3.0, required by hloc.
+# TODO(https://github.com/colmap/pycolmap/issues/111) use wheel when available for Python 3.10
 RUN git clone --branch v0.3.0 --recursive https://github.com/colmap/pycolmap.git && \
     cd pycolmap && \
     python3.10 -m pip install . && \

nerfstudio/cameras/cameras.py
@@ -19,7 +19,6 @@ import base64
 import math
 import os
 from dataclasses import dataclass
-from distutils.util import strtobool
 from enum import Enum, auto
 from typing import Dict, List, Optional, Tuple, Union
 
@@ -34,6 +33,7 @@ import nerfstudio.utils.poses as pose_utils
 from nerfstudio.cameras import camera_utils
 from nerfstudio.cameras.rays import RayBundle
 from nerfstudio.data.scene_box import SceneBox
+from nerfstudio.utils.misc import strtobool
 from nerfstudio.utils.tensor_dataclass import TensorDataclass
 
 

nerfstudio/data/dataparsers/nuscenes_dataparser.py
@@ -20,6 +20,7 @@ from pathlib import Path
 from typing import Optional, Tuple, Type
 
 import numpy as np
+import pyquaternion
 import torch
 from nuscenes.nuscenes import NuScenes as NuScenesDatabase
 from typing_extensions import Literal
@@ -31,14 +32,19 @@ from nerfstudio.data.dataparsers.base_dataparser import (
     DataparserOutputs,
 )
 from nerfstudio.data.scene_box import SceneBox
-from nerfstudio.process_data.colmap_utils import qvec2rotmat
 
 
-def rotation_translation_to_pose(r_vec, t_vec):
+def rotation_translation_to_pose(r_quat, t_vec):
     """Convert quaternion rotation and translation vectors to 4x4 matrix"""
 
     pose = np.eye(4)
-    pose[:3, :3] = qvec2rotmat(r_vec)
+
+    # NB: Nuscenes recommends pyquaternion, which uses scalar-first format (w x y z)
+    # https://github.com/nutonomy/nuscenes-devkit/issues/545#issuecomment-766509242
+    # https://github.com/KieranWynn/pyquaternion/blob/99025c17bab1c55265d61add13375433b35251af/pyquaternion/quaternion.py#L299
+    # https://fzheng.me/2017/11/12/quaternion_conventions_en/
+    pose[:3, :3] = pyquaternion.Quaternion(r_quat).rotation_matrix
+
     pose[:3, 3] = t_vec
     return pose
 

nerfstudio/data/dataparsers/phototourism_dataparser.py
@@ -33,7 +33,13 @@ from nerfstudio.data.dataparsers.base_dataparser import (
     DataparserOutputs,
 )
 from nerfstudio.data.scene_box import SceneBox
-from nerfstudio.data.utils.colmap_utils import read_cameras_binary, read_images_binary
+
+# TODO(1480) use pycolmap instead of colmap_parsing_utils
+# import pycolmap
+from nerfstudio.data.utils.colmap_parsing_utils import (
+    read_cameras_binary,
+    read_images_binary,
+)
 
 CONSOLE = Console(width=120)
 
@@ -76,11 +82,14 @@ class Phototourism(DataParser):
 
     # pylint: disable=too-many-statements
     def _generate_dataparser_outputs(self, split="train"):
-
         image_filenames = []
         poses = []
 
         with CONSOLE.status(f"[bold green]Reading phototourism images and poses for {split} split...") as _:
+            # TODO(1480) use pycolmap
+            # recon = pycolmap.Reconstruction(self.data / "dense" / "sparse")
+            # cams = recon.cameras
+            # imgs = recon.images
             cams = read_cameras_binary(self.data / "dense/sparse/cameras.bin")
             imgs = read_images_binary(self.data / "dense/sparse/images.bin")
 

nerfstudio/data/utils/colmap_parsing_utils.py
@@ -1,3 +1,11 @@
+"""
+This file copied with small modifications from:
+ * https://github.com/colmap/colmap/blob/1a4d0bad2e90aa65ce997c9d1779518eaed998d5/scripts/python/read_write_model.py
+
+TODO(1480) Delete this file when moving to pycolmap.
+
+"""
+
 # Copyright (c) 2018, ETH Zurich and UNC Chapel Hill.
 # All rights reserved.
 #

commit 30e7ea2b21f2a44637c454832b48ce6a2d404af8

Add support for more colmap camera models in ns-process-data (#1500)
* Add support for more colmap camera models in ns-process-data

* Fix bugs introduced in #1457

nerfstudio/process_data/colmap_utils.py
@@ -18,7 +18,7 @@ Tools supporting the execution of COLMAP and preparation of COLMAP-based dataset
 
 import json
 from pathlib import Path
-from typing import Dict, Optional
+from typing import Any, Dict, Optional
 
 import appdirs
 import cv2
@@ -182,10 +182,212 @@ def run_colmap(
     CONSOLE.log("[bold green]:tada: Done refining intrinsics.")
 
 
+def parse_colmap_camera_params(camera) -> Dict[str, Any]:  # pylint: disable=too-many-statements
+    """
+    Parses all currently supported COLMAP cameras into the transforms.json metadata
+
+    Args:
+        camera: COLMAP camera
+    Returns:
+        transforms.json metadata containing camera's intrinsics and distortion parameters
+
+    """
+    out: Dict[str, Any] = {
+        "w": camera.width,
+        "h": camera.height,
+    }
+
+    # Parameters match https://github.com/colmap/colmap/blob/dev/src/base/camera_models.h
+    camera_params = camera.params
+    if camera.model == "SIMPLE_PINHOLE":
+        # du = 0
+        # dv = 0
+        out["fl_x"] = float(camera_params[0])
+        out["fl_y"] = float(camera_params[0])
+        out["cx"] = float(camera_params[1])
+        out["cy"] = float(camera_params[2])
+        out["k1"] = 0.0
+        out["k2"] = 0.0
+        out["p1"] = 0.0
+        out["p2"] = 0.0
+        camera_model = CameraModel.OPENCV
+    elif camera.model == "PINHOLE":
+        # f, cx, cy, k
+
+        # du = 0
+        # dv = 0
+        out["fl_x"] = float(camera_params[0])
+        out["fl_y"] = float(camera_params[1])
+        out["cx"] = float(camera_params[2])
+        out["cy"] = float(camera_params[3])
+        out["k1"] = 0.0
+        out["k2"] = 0.0
+        out["p1"] = 0.0
+        out["p2"] = 0.0
+        camera_model = CameraModel.OPENCV
+    elif camera.model == "SIMPLE_RADIAL":
+        # f, cx, cy, k
+
+        # r2 = u**2 + v**2;
+        # radial = k * r2
+        # du = u * radial
+        # dv = u * radial
+        out["fl_x"] = float(camera_params[0])
+        out["fl_y"] = float(camera_params[0])
+        out["cx"] = float(camera_params[1])
+        out["cy"] = float(camera_params[2])
+        out["k1"] = float(camera_params[3])
+        out["k2"] = 0.0
+        out["p1"] = 0.0
+        out["p2"] = 0.0
+        camera_model = CameraModel.OPENCV
+    elif camera.model == "RADIAL":
+        # f, cx, cy, k1, k2
+
+        # r2 = u**2 + v**2;
+        # radial = k1 * r2 + k2 * r2 ** 2
+        # du = u * radial
+        # dv = v * radial
+        out["fl_x"] = float(camera_params[0])
+        out["fl_y"] = float(camera_params[0])
+        out["cx"] = float(camera_params[1])
+        out["cy"] = float(camera_params[2])
+        out["k1"] = float(camera_params[3])
+        out["k2"] = float(camera_params[4])
+        out["p1"] = 0.0
+        out["p2"] = 0.0
+        camera_model = CameraModel.OPENCV
+    elif camera.model == "OPENCV":
+        # fx, fy, cx, cy, k1, k2, p1, p2
+
+        # uv = u * v;
+        # r2 = u**2 + v**2
+        # radial = k1 * r2 + k2 * r2 ** 2
+        # du = u * radial + 2 * p1 * u*v + p2 * (r2 + 2 * u**2)
+        # dv = v * radial + 2 * p2 * u*v + p1 * (r2 + 2 * v**2)
+        out["fl_x"] = float(camera_params[0])
+        out["fl_y"] = float(camera_params[1])
+        out["cx"] = float(camera_params[2])
+        out["cy"] = float(camera_params[3])
+        out["k1"] = float(camera_params[4])
+        out["k2"] = float(camera_params[5])
+        out["p1"] = float(camera_params[6])
+        out["p2"] = float(camera_params[7])
+        camera_model = CameraModel.OPENCV
+    elif camera.model == "OPENCV_FISHEYE":
+        # fx, fy, cx, cy, k1, k2, k3, k4
+
+        # r = sqrt(u**2 + v**2)
+
+        # if r > eps:
+        #    theta = atan(r)
+        #    theta2 = theta ** 2
+        #    theta4 = theta2 ** 2
+        #    theta6 = theta4 * theta2
+        #    theta8 = theta4 ** 2
+        #    thetad = theta * (1 + k1 * theta2 + k2 * theta4 + k3 * theta6 + k4 * theta8)
+        #    du = u * thetad / r - u;
+        #    dv = v * thetad / r - v;
+        # else:
+        #    du = dv = 0
+        out["fl_x"] = float(camera_params[0])
+        out["fl_y"] = float(camera_params[1])
+        out["cx"] = float(camera_params[2])
+        out["cy"] = float(camera_params[3])
+        out["k1"] = float(camera_params[4])
+        out["k2"] = float(camera_params[5])
+        out["k3"] = float(camera_params[6])
+        out["k4"] = float(camera_params[7])
+        camera_model = CameraModel.OPENCV_FISHEYE
+    elif camera.model == "FULL_OPENCV":
+        # fx, fy, cx, cy, k1, k2, p1, p2, k3, k4, k5, k6
+
+        # u2 = u ** 2
+        # uv = u * v
+        # v2 = v ** 2
+        # r2 = u2 + v2
+        # r4 = r2 * r2
+        # r6 = r4 * r2
+        # radial = (1 + k1 * r2 + k2 * r4 + k3 * r6) /
+        #          (1 + k4 * r2 + k5 * r4 + k6 * r6)
+        # du = u * radial + 2 * p1 * uv + p2 * (r2 + 2 * u2) - u
+        # dv = v * radial + 2 * p2 * uv + p1 * (r2 + 2 * v2) - v
+        out["fl_x"] = float(camera_params[0])
+        out["fl_y"] = float(camera_params[1])
+        out["cx"] = float(camera_params[2])
+        out["cy"] = float(camera_params[3])
+        out["k1"] = float(camera_params[4])
+        out["k2"] = float(camera_params[5])
+        out["p1"] = float(camera_params[6])
+        out["p2"] = float(camera_params[7])
+        out["k3"] = float(camera_params[8])
+        out["k4"] = float(camera_params[9])
+        out["k5"] = float(camera_params[10])
+        out["k6"] = float(camera_params[11])
+        raise NotImplementedError(f"{camera.model} camera model is not supported yet!")
+    elif camera.model == "FOV":
+        # fx, fy, cx, cy, omega
+        out["fl_x"] = float(camera_params[0])
+        out["fl_y"] = float(camera_params[1])
+        out["cx"] = float(camera_params[2])
+        out["cy"] = float(camera_params[3])
+        out["omega"] = float(camera_params[4])
+        raise NotImplementedError(f"{camera.model} camera model is not supported yet!")
+    elif camera.model == "SIMPLE_RADIAL_FISHEYE":
+        # f, cx, cy, k
+
+        # r = sqrt(u ** 2 + v ** 2)
+        # if r > eps:
+        #     theta = atan(r)
+        #     theta2 = theta ** 2
+        #     thetad = theta * (1 + k * theta2)
+        #     du = u * thetad / r - u;
+        #     dv = v * thetad / r - v;
+        # else:
+        #     du = dv = 0
+        out["fl_x"] = float(camera_params[0])
+        out["fl_y"] = float(camera_params[0])
+        out["cx"] = float(camera_params[1])
+        out["cy"] = float(camera_params[2])
+        out["k1"] = float(camera_params[3])
+        out["k2"] = 0.0
+        out["k3"] = 0.0
+        out["k4"] = 0.0
+        camera_model = CameraModel.OPENCV_FISHEYE
+    elif camera.model == "RADIAL_FISHEYE":
+        # f, cx, cy, k1, k2
+
+        # r = sqrt(u ** 2 + v ** 2)
+        # if r > eps:
+        #     theta = atan(r)
+        #     theta2 = theta ** 2
+        #     theta4 = theta2 ** 2
+        #     thetad = theta * (1 + k * theta2)
+        #     thetad = theta * (1 + k1 * theta2 + k2 * theta4)
+        #     du = u * thetad / r - u;
+        #     dv = v * thetad / r - v;
+        # else:
+        #     du = dv = 0
+        out["fl_x"] = float(camera_params[0])
+        out["fl_y"] = float(camera_params[0])
+        out["cx"] = float(camera_params[1])
+        out["cy"] = float(camera_params[2])
+        out["k1"] = float(camera_params[3])
+        out["k2"] = float(camera_params[4])
+        out["k3"] = 0
+        out["k4"] = 0
+        camera_model = CameraModel.OPENCV_FISHEYE
+    else:
+        # THIN_PRISM_FISHEYE not supported!
+        raise NotImplementedError(f"{camera.model} camera model is not supported yet!")
+
+    out["camera_model"] = camera_model.value
+    return out
+
+
 def colmap_to_json(
     recon_dir: Path,
     output_dir: Path,
-    camera_model: CameraModel,
     camera_mask_path: Optional[Path] = None,
     image_id_to_depth_path: Optional[Dict[int, Path]] = None,
     image_rename_map: Optional[Dict[str, str]] = None,
@@ -210,10 +412,6 @@ def colmap_to_json(
     # im_id_to_image = recon.images
     cam_id_to_camera = read_cameras_binary(recon_dir / "cameras.bin")
     im_id_to_image = read_images_binary(recon_dir / "images.bin")
-
-    # Only support first camera
-    CAMERA_ID = 1  # pylint: disable=invalid-name
-    camera_params = cam_id_to_camera[CAMERA_ID].params
 
     frames = []
     for im_id, im_data in im_id_to_image.items():
@@ -252,35 +450,9 @@ def colmap_to_json(
             frame["depth_file_path"] = str(depth_path)
         frames.append(frame)
 
-    out = {
-        "fl_x": float(camera_params[0]),
-        "fl_y": float(camera_params[1]),
-        "cx": float(camera_params[2]),
-        "cy": float(camera_params[3]),
-        "w": cam_id_to_camera[CAMERA_ID].width,
-        "h": cam_id_to_camera[CAMERA_ID].height,
-        "camera_model": camera_model.value,
-    }
-
-    if camera_model == CameraModel.OPENCV:
-        out.update(
-            {
-                "k1": float(camera_params[4]),
-                "k2": float(camera_params[5]),
-                "p1": float(camera_params[6]),
-                "p2": float(camera_params[7]),
-            }
-        )
-    if camera_model == CameraModel.OPENCV_FISHEYE:
-        out.update(
-            {
-                "k1": float(camera_params[4]),
-                "k2": float(camera_params[5]),
-                "k3": float(camera_params[6]),
-                "k4": float(camera_params[7]),
-            }
-        )
-
+    if set(cam_id_to_camera.keys()) != {1}:
+        raise RuntimeError("Only single camera shared for all images is supported.")
+    out = parse_colmap_camera_params(cam_id_to_camera[1])
     out["frames"] = frames
 
     with open(output_dir / "transforms.json", "w", encoding="utf-8") as f:

scripts/process_data.py
@@ -182,15 +182,14 @@ class ProcessImages:
                 )
             )
         else:
-            image_id_to_depth_path = {}
+            image_id_to_depth_path = None
 
         # Save transforms.json
         if (colmap_model_path / "cameras.bin").exists():
             with CONSOLE.status("[bold yellow]Saving results to transforms.json", spinner="balloon"):
                 num_matched_frames = colmap_utils.colmap_to_json(
-                    recon_dir=colmap_dir / "sparse" / "0",
+                    recon_dir=colmap_model_path,
                     output_dir=self.output_dir,
-                    camera_model=CAMERA_MODELS[self.camera_type],
                     image_id_to_depth_path=image_id_to_depth_path,
                     image_rename_map=image_rename_map,
                 )
@@ -424,7 +423,6 @@ class ProcessVideo:
                 num_matched_frames = colmap_utils.colmap_to_json(
                     recon_dir=colmap_dir / "sparse" / "0",
                     output_dir=self.output_dir,
-                    camera_model=CAMERA_MODELS[self.camera_type],
                     image_id_to_depth_path=image_id_to_depth_path,
                     camera_mask_path=mask_path,
                     image_rename_map=None,
@@ -568,7 +566,6 @@ class ProcessInsta360:
                 num_matched_frames = colmap_utils.colmap_to_json(
                     recon_dir=colmap_dir / "sparse" / "0",
                     output_dir=self.output_dir,
-                    camera_model=CAMERA_MODELS["fisheye"],
                     image_id_to_depth_path=image_id_to_depth_path,
                     image_rename_map=None,
                 )